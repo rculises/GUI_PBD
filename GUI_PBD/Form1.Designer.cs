c54995b11073d4f5f876ec1227ad311dae6e8d79
Ulises <ulises.ramos.chavarria@gmail.com>
2021-09-28 11:57:50 -0500

    Se diseñó el menú principal de la aplicación.

--- GUI_PBD/Form1.Designer.cs
+++ GUI_PBD/Form1.Designer.cs
@@ -29,13 +29,84 @@ namespace GUI_PBD
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Poppins SemiBold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(56, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(424, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BIENVENIDOS AL SISTEMA DE CONTROL DE ALUMNOS Y EMPRESAS";
+            // 
+            // button3
+            // 
+            this.button3.Image = global::GUI_PBD.Properties.Resources.AlumnoEmpresa;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button3.Location = new System.Drawing.Point(343, 52);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 90);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Alumno - Empresa";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Image = global::GUI_PBD.Properties.Resources.Alumno;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button2.Location = new System.Drawing.Point(216, 52);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 90);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Alumno";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Image = global::GUI_PBD.Properties.Resources.Empresa;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.Location = new System.Drawing.Point(88, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 90);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Empresa";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(534, 203);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sistema PBD";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
